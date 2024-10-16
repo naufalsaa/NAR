429c24452cb20098f2e640f210f7df83c5b1f0af
naufalsaa <akbari.naufal@gmail.com>
2024-10-16 15:47:46 +0700

    feat folder architecture

---
+++ NAR/Presentation/Onboarding/View/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  NAR
+//
+//  Created by Naufal Satya Akbari on 16/10/24.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
